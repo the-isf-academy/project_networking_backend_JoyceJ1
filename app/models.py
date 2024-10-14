bb9b5526af798364a7306b4a7b018ad533b5b5a0
JoyceJ1 <4923002@student.isf.edu.hk>
2024-10-15 00:56:42 +0800

    I worked on some more, but got quite confused on the 'current scavenger_hunt' part and also whether i should (if yes, how) to add another user database.

--- app/models.py
+++ app/models.py
@@ -1,45 +1,86 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+import datetime
+
+
 
 class Scavenger_hunt(Model):
     # user_posted = StringField()
-    location = IntegerField()
-    # hint = StringField()
+    location = StringField()
+    hint = StringField()
     # image = BlobField()
     description = StringField()
-    # time_limit = IntegerField()
+    year = IntegerField()
+    month = IntegerField*()
+    day = IntegerField()
+    time_limit = BooleanField()
     code = StringField()
     complete = BooleanField()
     active = BooleanField()
     # user_completed = StringField()
     # comments = StringField()
     likes = IntegerField()
+    
 
     def json_response(self):
 
         return {
-            'user_posted' : self.user_posted,
+            # 'user_posted' : self.user_posted,
             'description' : self.description,
             # 'image' : self.image,
-            # 'time_limit' : self.time_limit,
+            'time_limit' : print(f"{self.year}-{self.month}-{self.day}"),
             # 'comments' : self.comments,
             'likes' : self.likes,
         }
     
-    def print_location(self, block, floor, room):
-        self.location = print(f'{block}{floor}{room}')
+    def hint_response(self):
+
+        return {
+
+            'description' : self.description,
+            'hint' : self.hint,
+            # 'image' : self.image,
+            'time_limit' : print(f"{self.year}-{self.month}-{self.day}"),
+            # 'comments' : self.comments,
+            'likes' : self.likes,
+        }
 
     def increase_likes(self):
+        #add likes by 1
         self.likes += 1
         self.save()
 
+    def create_code(self):
+        if len(self.code) < 5:
+            print("Your code needs to be at least 6 letters long!")
+
     def check_code(self, user_code):
+        #checks if the code is correct
         if self.code == user_code:
             print("Congratulations!")
             self.complete = True
             self.active = False
         else:
             print("Not Correct...")
+        self.save()
+    
+    def calculate_time(self, current_time):
+        current_time = datetime.datetime.now()
+        #checks the year
+        if current_time.year > self.year:
+            self.time_limit = False
+        #checks the month
+        else:
+            if current_time.month > self.month:
+                self.time_limit = False
+            #checks the day
+            else:
+                if current_time.day > self.month:
+                    self.time_limit = False
+        self.save()
 
-
+    def check_active(self):
+        if self.complete == False and self.time_limit == True:
+            self.active = True
+        self.save()
--- app/views.py
+++ app/views.py
@@ -2,4 +2,39 @@
 
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
+from .models import Scavenger_hunt
 
+@route_post(BASE_URL + 'new', args={'location':str, 'hint': str, 'description': str, 'year': int, 'month': int, 'day': int, 'code': str})
+def new_scavenger_hunt(args):
+    new_scavenger_hunt = Scavenger_hunt(
+        location = args['location'],
+        hint = args['hint'],
+        description = args['description'],
+        year = args['year'],
+        month = args['month'],
+        day = args['day'],
+        code = args['code'],
+        complete = False,
+        active = True,
+        likes = 0
+    )
+
+    new_scavenger_hunt.save()
+
+    return {'scavenger hunt': new_scavenger_hunt.json_response()}
+
+
+@route_get(BASE_URL + 'get')
+def get_scavenger_hunt(args):
+    if Scavenger_hunt.active == True:
+        get_scavenger_hunt = Scavenger_hunt.objects.order_by('?')
+        return{'riddle':get_scavenger_hunt[0].json_response()}
+    
+
+@route_post(BASE_URL + 'like')
+def increase_like(args):
+    #if the user has a current scavenger hunt
+    current_scavenger_hunt = Scavenger_hunt.objects.exists()
+    current_scavenger_hunt.increase_likes()
+        
+    return{'fortune':current_scavenger_hunt.json_response()}
